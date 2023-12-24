afe79b33b009ef3d5f17b5ae5732ed5e24d49fc4
SHOKinator <148518400+SHOKinator@users.noreply.github.com>
2023-12-25 04:37:16 +0600

    Update Main.java
    Main menu for selection actions

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Select action: ");
+        int action = scan.nextInt();
+
+        switch (action) {
+            case 1:
+                //Your code 1
+                break;
+            case 2:
+                //Your code 2
+                break;
+            case 3:
+                //Your code 3
+                break;
+            case 4:
+                //Your code 4
+                break;
+            case 5:
+                //Your code 5
+                break;
+            case 6:
+                //Your code 6
+                break;
+            case 7:
+                //Your code 7
+                break;
+            default:
+                System.out.println("Unknown action");
+        }
     }
 }
