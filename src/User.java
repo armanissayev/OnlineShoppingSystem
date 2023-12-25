04033d5c39edd9e3bacb2879ee7e0b952dc917c8
SHOKinator <148518400+SHOKinator@users.noreply.github.com>
2023-12-25 06:55:25 +0600

    construct, get and set (id, name, balance)

--- src/User.java
+++ src/User.java
@@ -5,6 +5,38 @@ import java.util.ArrayList;
 public class User {
     private int id;
     private String name;
-    private int balance;
+    private double balance;
     private ArrayList<Order> list = new ArrayList<Order>();
+
+    public User() {
+    }
+
+    public User(int id, String name, Double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+
+    }
+
+    //getters and setters
+    public double getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
