3ab7c1ad2b7a511ed7e4b0eba862737f2a629dce
SHOKinator <148518400+SHOKinator@users.noreply.github.com>
2023-12-25 14:36:52 +0600

    Exit
    The program will not end until 8 is selected  in menu

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,68 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        while (true) {
+            Scanner scan = new Scanner(System.in);
 
-        System.out.println("Select action: ");
-        System.out.println("1) To show products list");
-        System.out.println("2) To add a product");
-        System.out.println("3) To add a new user");
-        System.out.println("4) To buy product");
-        System.out.println("5) To return a product");
-        System.out.println("6) To show all users");
-        System.out.println("7) To show the certain user’s orders");
-        
-        int action = scan.nextInt();
+            System.out.println("Select action: ");
+            System.out.println("1) To show products list");
+            System.out.println("2) To add a product");
+            System.out.println("3) To add a new user");
+            System.out.println("4) To buy product");
+            System.out.println("5) To return a product");
+            System.out.println("6) To show all users");
+            System.out.println("7) To show the certain user’s orders");
+            System.out.println("8) To exit");
 
-        switch (action) {
-            case 1:
-                //Your code 1
-                break;
-            case 2:
-                //Your code 2
-                break;
-            case 3:
-                System.out.println("Enter your id: ");
-                int id = scan.nextInt();
+            System.out.print("Choice: ");
+            int action = scan.nextInt();
 
-                String username;
-                do {
-                    System.out.println("Enter username: ");
-                    username = scan.nextLine();
-                } while (username.isEmpty());
+            switch (action) {
+                case 1:
+                    //Your code 1
+                    break;
+                case 2:
+                    //Your code 2
+                    break;
+                case 3:
+                    System.out.println("Enter your id: ");
+                    int id = scan.nextInt();
 
-                System.out.println("Enter your balance: ");
-                double money = scan.nextDouble();
+                    String username;
+                    do {
+                        System.out.println("Enter username: ");
+                        username = scan.nextLine();
+                    } while (username.isEmpty());
 
-                User user = new User(id, username, money);
+                    System.out.println("Enter your balance: ");
+                    double money = scan.nextDouble();
 
-                System.out.println("Checking...");
-                System.out.println("UserId: " + user.getId());
-                System.out.println("Username: " + user.getName());
-                System.out.println("Balance: " + user.getBalance());
-                System.out.println("You have successfully signed in!");
-                break;
-            case 4:
-                //Your code 4
-                break;
-            case 5:
-                //Your code 5
-                break;
-            case 6:
-                //Your code 6
-                break;
-            case 7:
-                //Your code 7
-                break;
-            default:
-                System.out.println("Unknown action");
-                break;
+                    User user = new User(id, username, money);
+
+                    System.out.println("Checking...");
+                    System.out.println("UserId: " + user.getId());
+                    System.out.println("Username: " + user.getName());
+                    System.out.println("Balance: " + user.getBalance());
+                    System.out.println("You have successfully signed in!\n\n");
+                    break;
+                case 4:
+                    //Your code 4
+                    break;
+                case 5:
+                    //Your code 5
+                    break;
+                case 6:
+                    //Your code 6
+                    break;
+                case 7:
+                    //Your code 7
+                    break;
+                case 8:
+                    System.out.println("Exit in progress...");
+                    System.exit(0);
+                default:
+                    System.out.println("Unknown action!");
+            }
         }
     }
 }
