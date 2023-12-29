e3a964d7a7136cf82f618e260a08a3e3f12c2ad7
Shokay <148518400+SHOKinator@users.noreply.github.com>
2023-12-29 20:22:19 +0600

    For product purchasing or returning system

--- src/Order.java
+++ src/Order.java
@@ -2,6 +2,44 @@ public class Order {
     private int userId;
     private String productName;
     private int quantity;
-    private int totalSum;
+    private double totalSum;
+    private Product product;
 
+    public Order(int userId, String productName, int quantity, double totalSum) {
+        this.userId = userId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.totalSum = totalSum;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public double getTotalSum() {
+        return totalSum;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setTotalSum(double totalSum) {
+        this.totalSum = totalSum;
+    }
+
+    public double calculateTotalCost() {
+        return quantity * product.getPrice();
+    }
 }
