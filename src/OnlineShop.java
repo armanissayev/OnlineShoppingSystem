d2117dafd06bd075e4612aaaf57c914be080d24f
Shokay <148518400+SHOKinator@users.noreply.github.com>
2024-01-23 21:42:22 +0500

    Primary & Ordinary

--- src/OnlineShop.java
+++ src/OnlineShop.java
@@ -1,3 +1,18 @@
-public class OnlineShop {
+import java.util.ArrayList;
 
+public class OnlineShop {
+    private ArrayList<PremiumUser> premiumUsers;
+
+    public OnlineShop() {
+        premiumUsers = new ArrayList<>();
+    }
+
+    public void addPremiumUser(PremiumUser premiumUser) {
+        premiumUsers.add(premiumUser);
+        System.out.println("Premium user added: " + premiumUser.getName());
+    }
+
+    public ArrayList<PremiumUser> getPremiumUsers() {
+        return premiumUsers;
+    }
 }
